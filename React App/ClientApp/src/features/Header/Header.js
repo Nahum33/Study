a30ee7cbe57ec3b4e9068a2747b05a78b2485f95
Nahum33 <55307587+Nahum33@users.noreply.github.com>
2023-06-17 21:01:40 -0600

    Adding styles
    Adding styles to the header navigator menu

--- React App/ClientApp/src/features/Header/Header.js
+++ React App/ClientApp/src/features/Header/Header.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './Header.css';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      menuOpen: !prevState.menuOpen,
+    }));
+  }
 
   render() {
+    const { menuOpen } = this.state;
+
     return (
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about-us">Acerca de Nosotros</a></li>
-        </ul>
+      
+      <nav className={menuOpen ? 'topnav responsive' : 'topnav'} id="myTopnav">
+        <a className='active' href="/">Home</a>
+        <a href="/about-us">Acerca de Nosotros</a>
+        <button className="icon" onClick={this.toggleMenu}>
+          <i class="fa fa-bars"></i>
+        </button>
       </nav>
     );
   }
