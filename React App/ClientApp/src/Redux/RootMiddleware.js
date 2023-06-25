3e57f0e5190d25545522a38e909191e57a46d727
Nahum33 <55307587+Nahum33@users.noreply.github.com>
2023-06-25 10:31:41 -0600

    Delete React App/ClientApp/src/Redux directory

--- React App/ClientApp/src/Redux/RootMiddleware.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware } from 'redux';
-import { productsMiddleware } from './middlewares/productsMiddleware';
-
-const composedMiddleware = applyMiddleware(productsMiddleware);
-
-export default composedMiddleware;
--- React App/ClientApp/src/Redux/Store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './RootReducer';
-import composedMiddleware from './RootMiddleware';
-
-const store = createStore(rootReducer, composedMiddleware);
-export default store;
